9d03d618565f2c8af5bd413405d8ca520060b6c1
WillRV <32873195+WillRV@users.noreply.github.com>
2021-06-26 18:21:44 -0300

    Nova versão do código da geladeira
    Agora ele recebe códigos IR na porta desejada e envia IR por função.

--- geladeira.ino
+++ geladeira.ino
@@ -1,28 +1,47 @@
 #include <IRremote.h>
 #define pinSensorPresenca 11
+#define ledIR 3
+int pinReceptorIR = 8;
 void enviarIR(int botao);
+void receberIR();
 
 //Habilitando send do IR
 IRsend irsend;
+//Instanciando receptor IR
+IRrecv irrecv(pinReceptorIR);
+decode_results results;
 
 void setup() {
   pinMode(pinSensorPresenca, INPUT);
   Serial.begin(9600);
-  irsend.begin(3, ENABLE_LED_FEEDBACK);
+  irrecv.enableIRIn(); // Inicializa o receptor iR
+  irsend.begin(ledIR, ENABLE_LED_FEEDBACK); //Diz qual porta o led IR está (para enviar o IR)
 }
 
-void loop() {
-  // Quando o Arduino detectar um sinal do sensor de presença, ele executará as seguintes ações
-  if(digitalRead(pinSensorPresenca) == HIGH){
-    Serial.println("Botao apertado");
-    enviarIR(1);
+void loop() { 
+  receberIR();
+  if(digitalRead(pinSensorPresenca) == HIGH){ // Quando o Arduino detectar um sinal do sensor de presença, ele executará as seguintes ações
+    enviarIR(2);
   }
-  //Fim sensor de presença
 }
 
 void enviarIR(int botao){
   if(botao == 1){
+    //Sinal para avançar
     irsend.sendNEC(0xBD30CF, 32);
-    delay(50);
+    delay(1000);
+  }else if (botao == 2){
+    //Sinal para retroceder
+    irsend.sendNEC(0xBD08F7, 32);
+    delay(1000);
   }
+}
+
+void receberIR(){
+  if (irrecv.decode(&results)) {
+    Serial.print("Valor recebido pelo IR: ");
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
 }
